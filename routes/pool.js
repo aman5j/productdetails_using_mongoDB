b7f9aec5d177327342daa44a0352dcfeb8685161
aman5j <amanprajapati190@gmail.com>
2026-05-20 17:07:00 +0530

    mongodb setup for deployement

--- routes/pool.js
+++ routes/pool.js
@@ -1,22 +1,44 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-var pool=()=>{
-    mongoose.Promise = global.Promise;
-    var options={}
+// var pool=()=>{
+//     mongoose.Promise = global.Promise;
+//     var options={}
     
-    mongoose.connect(
-        `mongodb://127.0.0.1:27017/productdetails?retryWrites=true&w=majority`,   
-         options
-       );
+//     mongoose.connect(
+//         `mongodb://127.0.0.1:27017/productdetails?retryWrites=true&w=majority`,   
+//          options
+//        );
 
-       mongoose.connection.
-       once("open", () => console.log("MongoDb running"))
+//        mongoose.connection.
+//        once("open", () => console.log("MongoDb running"))
        
-       .on("error", (err) => console.log(err));
+//        .on("error", (err) => console.log(err));
     
-   };
+//    };
 
 
 
 
-module.exports=pool
+// module.exports=pool
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbUrl = process.env.ATLASDB_URL;
+// console.log("connection String :::::",dbUrl);
+
+const pool = async () => {
+    try {
+
+        await mongoose.connect(dbUrl);
+
+        console.log("MongoDB Connected");
+
+    } catch (err) {
+
+        console.log("Database Error:", err);
+
+    }
+};
+
+module.exports = pool;
